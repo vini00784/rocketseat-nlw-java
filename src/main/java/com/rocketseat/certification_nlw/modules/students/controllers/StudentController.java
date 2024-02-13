3e2754f80d492afc6d4174003e38d77103e86c2b
vini00784 <vo70346@gmail.com>
2024-02-13 12:22:19 -0300

    student controller

---
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/controllers/StudentController.java
@@ -0,0 +1,29 @@
+package com.rocketseat.certification_nlw.modules.students.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rocketseat.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import com.rocketseat.certification_nlw.modules.students.useCases.VerifyIfHasCertificationUseCase;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    
+    // Preciso usar o meu useCase
+    @Autowired
+    private VerifyIfHasCertificationUseCase verifyIfHasCertificationUseCase;
+
+    @PostMapping("/verifyIfHasCertification")
+    public String verifyIfHasCertification(@RequestBody VerifyHasCertificationDTO verifyHasCertificationDTO) {
+        var result = this.verifyIfHasCertificationUseCase.execute(verifyHasCertificationDTO);
+        if(result)
+            return "Usuário já fez a prova!";
+        else
+            return "Usuário apto a fazer a prova!";
+    }
+
+}
